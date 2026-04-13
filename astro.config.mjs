31f43128651f715e63cb6a59f0b1759937f015f5
ThySummer14 <thaotuyetbauqciloc@gmail.com>
2026-04-13 23:11:53 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 
+// https://astro.build/config
 export default defineConfig({
-  // ✨ 核心修正：即便仓库名带点，base 的最后也必须补上斜杠
+  // ✨ 终极路由底座：斜杠封尾，雷打不动
   base: '/thysummer./', 
   integrations: [vue()],
 });
