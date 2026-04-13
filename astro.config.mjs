e88885b44562a26ec30a611e620baf2241383d10
ThySummer14 <thaotuyetbauqciloc@gmail.com>
2026-04-13 22:51:44 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 
-// https://astro.build/config
 export default defineConfig({
-  // ✨ 重点：末尾的斜杠是文件夹的分界线，必须保留
+  // ✨ 核心修正：即便仓库名带点，base 的最后也必须补上斜杠
   base: '/thysummer./', 
   integrations: [vue()],
-  // 如果你有其他配置（如 site），请保留在下方
 });
